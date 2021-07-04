829ae48e117ac708037c493f8824d90bb06d5bdd
Geordon Worley <vadixidav@gmail.com>
2021-07-04 03:21:47 -0500

    add cpu native to runrecall.sh

--- runrecall.sh
+++ runrecall.sh
@@ -1,1 +1,1 @@
-function runrecall { cargo run --release --example recall > "$1.csv" 2> >(tee "$1.log" >&2); }
+function runrecall { RUSTFLAGS="-Ctarget-cpu=native" cargo run --release --example recall > "$1.csv" 2> >(tee "$1.log" >&2); }
